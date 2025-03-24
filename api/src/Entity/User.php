cff7fdff9837422fecfef04ebdda7c5c7de6d609
Andrew Vozniuk <diguni_vav@ztu.edu.ua>
2025-03-24 20:05:04 +0200

    Saved data

---
+++ api/src/Entity/User.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+
+#[Entity]
+class User
+{
+    #[Id]
+    #[Column]
+    #[GeneratedValue]
+    private int $id;
+
+    #[Column(type: 'string', length: 255, nullable: true)]
+    private string $myName;
+
+    #[Column(type: 'string', length: 255, unique: true)]
+    private string $email;
+
+    #[Column(type: 'string', length: 255)]
+    private string $password;
+
+    /**
+     * @var Collection<int, Order>
+     */
+    #[ORM\OneToMany(targetEntity: Order::class, mappedBy: 'user')]
+    private Collection $orders;
+
+    public function __construct()
+    {
+        $this->orders = new ArrayCollection();
+    }
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getMyName(): string
+    {
+        return $this->myName;
+    }
+
+    public function setMyName(string $myName): User
+    {
+        $this->myName = $myName;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): User
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): User
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Order>
+     */
+    public function getOrders(): Collection
+    {
+        return $this->orders;
+    }
+
+    public function addOrder(Order $order): static
+    {
+        if (!$this->orders->contains($order)) {
+            $this->orders->add($order);
+            $order->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOrder(Order $order): static
+    {
+        if ($this->orders->removeElement($order)) {
+            // set the owning side to null (unless already changed)
+            if ($order->getUser() === $this) {
+                $order->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+}
