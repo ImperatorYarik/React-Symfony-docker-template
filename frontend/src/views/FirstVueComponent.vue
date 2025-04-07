c636d8408f7b0e852d9c66a4779a15f2509fbdff
Andrew Vozniuk <diguni_vav@ztu.edu.ua>
2025-04-07 18:07:26 +0300

    Saved frontend

---
+++ frontend/src/views/FirstVueComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>
+    Hello my first component
+  </h1>
+  <img src="../assets/images/italyDSC0101420241213202.jpg" alt="italy" style="width: 300px; height: auto">
+</template>
+
+<style scoped>
+
+</style>
