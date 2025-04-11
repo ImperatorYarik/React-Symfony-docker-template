68c0088c97954a6764c0df9b8fcb8a18771161bb
Andrew Vozniuk <diguni_vav@ztu.edu.ua>
2025-04-11 18:08:39 +0300

    [FEATURE] Rules

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -213,6 +213,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[Column(nullable: true)]
     private ?bool $isActive = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $avatar = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -315,5 +318,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getUserIdentifier(): string
     {
         return $this->email ?? $this->phoneNumber;
+    }
+
+    public function getAvatar(): ?string
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar(string $avatar): self
+    {
+        $this->avatar = $avatar;
+
+        return $this;
     }
 }
